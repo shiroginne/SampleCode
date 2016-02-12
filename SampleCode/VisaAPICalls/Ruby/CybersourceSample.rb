bc8477b8755cf86092e325c6e0f7f4fd9ef77703
Evgeny Sugakov <shiroginne@gmail.com>
2016-02-12 15:53:26 +0300

    Change CybersourceSample.rb to more idiomatic ruby code.

--- SampleCode/VisaAPICalls/Ruby/CybersourceSample.rb
+++ SampleCode/VisaAPICalls/Ruby/CybersourceSample.rb
@@ -1,45 +1,47 @@
 require 'restclient'
-  
-$api_key = "put your api key here"
-$shared_secret = "put your shared secret here"
-$base_uri = "cybersource/"
-$resource_path = "payments/v1/authorizations"
-$query_string = "apiKey=" + $api_key
+require 'digest'
+
+API_KEY = "put your api key here"
+SHARED_SECRET = "put your shared secret here"
+BASE_URL = "cybersource/"
+RESOURCE_PATH = "payments/v1/authorizations"
+QUERY_STRING = "apiKey=#{API_KEY}"
 
 def get_xpay_token(resource_path, query_string, request_body)
-  require 'digest'
-  timestamp = Time.now.getutc.to_i.to_s
-  hash_input = $shared_secret + timestamp + resource_path + query_string + request_body
+  timestamp = Time.now.utc.to_i
+  hash_input = "#{SHARED_SECRET}#{timestamp}#{resource_path}#{query_string}#{request_body}"
   hash_output = Digest::SHA256.hexdigest(hash_input)
-  return "x:" + timestamp + ":" + hash_output
+
+  "x:#{timestamp}:#{hash_output}"
 end
 
 def authorize_credit_card(request_body)
-  xpay_token = get_xpay_token($resource_path, $query_string, request_body)
-  full_request_url = "https://sandbox.api.visa.com/" + $base_uri + $resource_path + "?" + $query_string
+  xpay_token = get_xpay_token(RESOURCE_PATH, QUERY_STRING, request_body)
+  full_request_url = "https://sandbox.api.visa.com/#{BASE_URL}#{RESOURCE_PATH}?#{QUERY_STRING}"
+
   begin
-    response = RestClient::Request.execute(:url => full_request_url,
+    RestClient::Request.execute(
+      :url => full_request_url,
       :method => :post,
       :payload => request_body,
       :headers => {
         "content-type" => "application/json",
         "x-pay-token" => xpay_token
       }
-  )
+    )
   rescue RestClient::ExceptionWithResponse => e
-    response = e.response
+    e.response
   end
-  return response
 end
 
 request_body = {
-      "amount" => "0",
-      "currency" => "USD",
-      "payment" => {
-      "cardNumber" => "4111111111111111",
-      "cardExpirationMonth" => "10",
-      "cardExpirationYear" =>  "2016"
-      }
-  }.to_json;
-  
+  "amount" => "0",
+  "currency" => "USD",
+  "payment" => {
+    "cardNumber" => "4111111111111111",
+    "cardExpirationMonth" => "10",
+    "cardExpirationYear" =>  "2016"
+  }
+}.to_json
+
 puts authorize_credit_card(request_body)
